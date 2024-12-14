3352b71cacc9d1b143698ce5dce8a67a69a64e0a
Ilyas Ouacham Rhailane <ilyas.ouacham@estudiantat.upc.edu>
2024-12-14 20:21:46 +0100

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -6,8 +6,8 @@ class db_retriever:
         self.db = None
         self.db_name = "dbqr"
         self.users_collection = "dbqrcol"
-        self.preprocessed_data_collection = "pre_data"
-        self.knowledge_collection = "scraped_data"
+        self.knowledge_collection = "pre_data"
+        self.doctor_keys = "medkeys"
     
     def connect(self, db_name = "dbqr"):
         self.db = self.client[db_name]
@@ -21,17 +21,26 @@ class db_retriever:
         collection = self.get_collection(self.users_collection)
         return collection.find_one({f"{key}": f"{value}"})
     
-    def get_preprocessed_data(self):
-        collection = self.get_collection(self.preprocessed_data_collection)
-        return collection.find()
-
     def get_knowledge(self):
         collection = self.get_collection(self.knowledge_collection)
         return collection.find()
+
+    def doctor_keys(self, key):
+        collection = self.get_collection(self.doctor_keys)
+        return collection.find_one({"medic_key": f"{key}"})
 
     def close_connection(self):
         """Closes the connection to the MongoDB server."""
         self.client.close()
 
-if __name__ == "__main__":
-    pass
+
+"""
+EJEMPLO DE USO
+db = db_retriever("mongodb+srv://Ilyas:NlzFSgDrycE0gRGt@cluster0.9t4o9.mongodb.net/")
+db.connect()
+user1 = db.get_user("username", "Andres")
+
+print(user1['medical_info']['age'])
+
+db.close_connection()
+"""
